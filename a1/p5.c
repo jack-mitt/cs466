203ed6f95eb491934dab4c6f5a4c439385f6e817
jack <jackmittelmeier@email.arizona.edu>
2019-09-04 21:03:52 -0700

    fixed

--- a1/p5.c
+++ a1/p5.c
@@ -3,8 +3,8 @@
 int main(){
   uint32_t a = 0xaabbccdd;
   uint32_t b = 0x11223344;
-  uint32_t a_evens = a & 0xAAAAAAAA;
-  b = b & 0x55555555;
+  uint32_t a_evens = a & 0x55555555;
+  b = b & 0xAAAAAAAA;
   b = b | a_evens;
   printf("%x\n", b);
   return 0;
